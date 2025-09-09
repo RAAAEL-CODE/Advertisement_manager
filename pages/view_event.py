d1f170e9d149291dd6c405995454e7a851d0884b
Alexa Fosu <alexandria.fosu@gmail.com>
2025-09-09 15:52:21 +0000

    view Event

--- pages/view_event.py
+++ pages/view_event.py
@@ -1,4 +1,16 @@
 from nicegui import ui 
 
 def show_view_page():
+    goods =["funiture"]
+    with ui.column():
+        ui.select(label="category", options=["All" "funiture"], on_change=lambda e: filter_goods(e.value))
+        for good in goods:
+            with ui.column():
+                ui.image("Assets/the-last-shirt-1510597_1280.jpg").classes("w-[400px] h-1/2 rounded-lg shadow-xl transition-transform duration-300 hover:scale-110")
+                ui.label("Euro Shirt").classes("text-xl font-semibold")
+                ui.label("price:ghc 150").classes("text-gray-700 text-xl shadow-xl")
+                ui.label("Description: A single mordern sofa with plush cushions-Elastic silky fiber fabric. Available in different colors. visit shop for a 50% discount ").classes("items-center")
+def filter_goods(category):
+    pass                
+            
     ui.label ('This is the view page')
