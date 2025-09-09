7e2ca627fc330c967dae5e20340cd6fdc18b7bdf
Alexa Fosu <alexandria.fosu@gmail.com>
2025-09-09 11:45:57 +0000

    home page update

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from nicegui import ui 
+from nicegui import ui,app
 from components.header import show_header
 from pages.home import show_home_page
 from pages.add_event import show_add_event_page
 from pages.edit_event import show_edit_event_page
 from pages.view_event import show_view_page
 
+app.add_static_files("/Assets", "Assets")
 
 @ui.page("/")
 def home_page():
@@ -20,7 +21,7 @@ def add_event_page():
 def view_event_page():
     show_header()
     show_view_page()
-    
+
 @ui.page("/edit_event")
 def edit_event_page():
     show_edit_event_page()
