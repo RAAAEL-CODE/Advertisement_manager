69c358d0759538887fd8f1370b8322a22f88e9d7
rukkyy <rukayamohammed448@gmail.com>
2025-09-10 13:33:34 +0000

    Changes to homepage

--- pages/welcome.py
+++ pages/welcome.py
@@ -1,7 +1,10 @@
 from nicegui import ui
 
 def show_welcome_page():
-   with ui.row().classes("w-screen h-screen"):
-    ui.video("Assets/Pink Retro Welcome to my Channel Youtube Video.mp4", autoplay=True, loop=True, muted=True,controls=False)
+   with ui.row().classes("w-screen h-screen relative"):
+    ui.video("Assets/Pink Retro Welcome to my Channel Youtube Video.mp4", autoplay=True,
+             loop=True,
+               muted=True,
+               controls=False).classes("absolute insert-0 w-full h-full object-cover z-0")
 
 
