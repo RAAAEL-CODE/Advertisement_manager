639b41ba905285d5080a912791f010b6402f8808
Nijaycrypt22 <ayehanita@gmail.com>
2025-09-10 11:33:41 +0000

    updated add and edit

--- pages/add_event.py
+++ pages/add_event.py
@@ -1,13 +1,18 @@
 from nicegui import ui 
 
 def show_add_event_page():
+    with ui.column().classes('max-w-2xl mx-auto rounded-lg shadow-xl w-full'):
+        ui.label ('CREATE A NEW ADVERT').classes('text-bold mb-6  text-3xl tracking-wider text-center self-center')
+        ui.input('Title').props('outlined').classes('w-full')
+        ui.textarea('Item Description').props('outlined').classes('w-full')
+        ui.number('Price').props('outlined').classes('w-full')
+        ui.label('Categories').classes('font-light mb-2')
+        ui.select(['CLOTHINGS','TUMBLERS','CARS', 'FURNITURE']).classes("w-[670px]").props('outlined')
+        ui.label('Upload image').classes('font-light mb-2')
+        ui.upload(on_upload=lambda e: ui.notify(f'Uploaded'),
+        on_rejected=lambda: ui.notify('Rejected!'),
+        max_file_size=5_000_000).classes('w-[670px]')
     
-    ui.label ('CREATE A NEW ADVERT').classes('text-bold text-3xl tracking-wider items-center')
-    Title=ui.input('Title').props('outlined').classes('w-1/2 items-center')
-    Description=ui.textarea('Item Description').props('outlined').classes('w-1/2 items-center')
-    Price=ui.number('Price').props('outlined').classes('w-1/2 items-center')
-    ui.select(['Upload image from camera roll','Browse',]).classes("w-[670px] items-center").props('outlined')
-    
-    def submit():
-        ui.notify(f'Add Submitted Successfully: {Title.value}')
-    ui.button('Submit', on_click=submit).classes('mt-4 bg-black text-white justify-center items-center w-[200px]')    
+        def submit():
+            ui.notify(f'Add Submitted Successfully')
+    ui.button('Submit', on_click=submit).classes('mt-4 bg-black text-white w-[200px] self-center')    
