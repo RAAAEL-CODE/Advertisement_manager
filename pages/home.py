08ca55240c3224e407f3a192b98e6d40918ea52a
Alexa Fosu <alexandria.fosu@gmail.com>
2025-09-10 11:32:14 +0000

    view page update

--- pages/home.py
+++ pages/home.py
@@ -1,51 +1,76 @@
 from nicegui import ui 
-
+       
 def show_home_page():
    with ui.grid(columns=3).classes("w-screen h-screen justify-center p-8 gap-12 mx-8 items-start"):
         with ui.column().classes("w-72 items-center"):
             ui.image("Assets/furniture-5966893_1280.jpg").classes("w-[400px] h-1/2 rounded-lg shadow-xl transition-transform duration-300 hover:scale-110")
             ui.label("single sofa").classes("mt-4 text-4xl font-semibold")
             ui.label("price:ghc 1000").classes("text-gray-700 text-xl shadow-xl")
-            ui.button("view")
+            url = 'http://127.0.0.1:8080/view_event'
+            ui.button('view', on_click=lambda: ui.navigate.to(url))
 
         with ui.column().classes("w-72 items-center"):
             ui.image("Assets/the-last-shirt-1510597_1280.jpg").classes("w-[400px] h-1/2 rounded-lg shadow-xl transition-transform duration-300 hover:scale-110")
             ui.label("Euro Shirt").classes("mt-4 text-4xl font-semibold")
             ui.label("price:ghc 150").classes("text-gray-700 text-xl shadow-xl")
-            ui.button("view")        
+            url = 'http://127.0.0.1:8080/view_event'
+            ui.button('view', on_click=lambda: ui.navigate.to(url))
+
+    
         with ui.column().classes("w-72 items-center"):
             ui.image("Assets/fashion-6203956_1280.jpg").classes("w-[400px] h-1/2 rounded-lg shadow-xltransition-transform duration-300 hover:scale-110")
             ui.label("Gown").classes("mt-4 text-4xl font-semibold")
             ui.label("price:ghc 5000").classes("text-gray-700 text-xl shadow-xl")
-            ui.button("view")
+            url = 'http://127.0.0.1:8080/view_event'
+            ui.button('view', on_click=lambda: ui.navigate.to(url))
+
+
         with ui.column().classes("w-72 items-center"):
             ui.image("Assets/water-bottle-898332_1280.jpg").classes("w-[400px] h-1/2 rounded-lg shadow-xl transition-transform duration-300 hover:scale-110")
             ui.label("Water-bottle").classes("mt-4 text-4xl font-semibold")
             ui.label("price:ghc 50").classes("text-gray-700 text-xl shadow-xl")
-            ui.button("view")  
+            url = 'http://127.0.0.1:8080/view_event'
+            ui.button('view', on_click=lambda: ui.navigate.to(url))
+
+ 
         with ui.column().classes("w-72 items-center"):
             ui.image("Assets/travel-bag-4326732_1280.jpg").classes("w-[400px] h-1/2 rounded-lg shadow-xl transition-transform duration-300 hover:scale-110")
             ui.label("Travel Bag").classes("mt-4 text-4xl font-semibold")
             ui.label("price:ghc 700").classes("text-gray-700 text-xl shadow-xl")
-            ui.button("view")    
+            url = 'http://127.0.0.1:8080/view_event'
+            ui.button('view', on_click=lambda: ui.navigate.to(url))
+
+ 
         with ui.column().classes("w-72 items-center"):
             ui.image("Assets/vehicle-2132360_1280.jpg").classes("w-[400px] h-1/2 rounded-lg shadow-xl transition-transform duration-300 hover:scale-110")
             ui.label("1987 Vintage car").classes("mt-4 text-4xl font-semibold")
             ui.label("price:ghc 3000000").classes("text-gray-700 text-xl shadow-xl")
-            ui.button("view")    
+            url = 'http://127.0.0.1:8080/view_event'
+            ui.button('view', on_click=lambda: ui.navigate.to(url))
+
+  
         with ui.column().classes("w-72 items-center"):
             ui.image("Assets/bag-1052370_1280.jpg").classes("w-[400px] h-1/2 rounded-lg shadow-xl transition-transform duration-300 hover:scale-110")
             ui.label("Ladies bag").classes("mt-4 text-4xl font-semibold")
             ui.label("price:ghc 400").classes("text-gray-700 text-xl shadow-xl")
-            ui.button("view")    
+            url = 'http://127.0.0.1:8080/view_event'
+            ui.button('view', on_click=lambda: ui.navigate.to(url))
+
+
         with ui.column().classes("w-72 items-center"):
             ui.image("Assets/chairs-2181960_1280.jpg").classes("w-[400px] h-1/2 rounded-lg shadow-xl transition-transform duration-300 hover:scale-110")
             ui.label("Dinning table set").classes("mt-4 text-4xl font-semibold")
             ui.label("price:ghc 3000").classes("text-gray-700 text-xl shadow-xl")
-            ui.button("view")    
+            url = 'http://127.0.0.1:8080/view_event'
+            ui.button('view', on_click=lambda: ui.navigate.to(url))
+   
+
         with ui.column().classes("w-72 items-center"):
             ui.image("Assets/mug-5161566_1280.jpg").classes("w-[400px] h-1/2 rounded-lg shadow-xl transition-transform duration-300 hover:scale-110")
             ui.label("Mug").classes("mt-4 text-4xl font-semibold")
             ui.label("price:ghc 80").classes("text-gray-700 text-xl shadow-xl")
-            ui.button("view")    
+            url = 'http://127.0.0.1:8080/view_event'
+            ui.button('view', on_click=lambda: ui.navigate.to(url))
+   
+                                    
                                     
--- pages/view_event.py
+++ pages/view_event.py
@@ -1,16 +1,11 @@
 from nicegui import ui 
 
 def show_view_page():
-    goods =["funiture"]
-    with ui.column():
-        ui.select(label="category", options=["All" "funiture"], on_change=lambda e: filter_goods(e.value))
-        for good in goods:
-            with ui.column():
-                ui.image("Assets/the-last-shirt-1510597_1280.jpg").classes("w-[400px] h-1/2 rounded-lg shadow-xl transition-transform duration-300 hover:scale-110")
-                ui.label("Euro Shirt").classes("text-xl font-semibold")
-                ui.label("price:ghc 150").classes("text-gray-700 text-xl shadow-xl")
-                ui.label("Description: A single mordern sofa with plush cushions-Elastic silky fiber fabric. Available in different colors. visit shop for a 50% discount ").classes("items-center")
-def filter_goods(category):
-    pass                
+    
+    with ui.card().classes("w-full h-full sm:w-[90%] md:w-[70%] lg:w-[60%] mx-auto p-6 bg-white rounde-xl shadow-lg"):
+        ui.image("Assets/the-last-shirt-1510597_1280.jpg").classes("w-full h-48 sm:h-60 rounded-lg mb-4 shadow-xl transition-transform duration-300 hover:scale-110")
+        ui.label("Euro Shirt").classes("text-xl font-semibold")
+        ui.label("price:ghc 150").classes("text-gray-700 text-xl shadow-xl")
+        ui.label("category: Fashion")
+        ui.label("Description: A single mordern sofa with plush cushions-Elastic silky fiber fabric. Available in different colors. visit shop for a 50% discount ").classes("text-grey-600 mb-2")
             
-    ui.label ('This is the view page')
