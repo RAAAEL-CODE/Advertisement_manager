47c3b29e740b768738b5094346ce32a6ae220e02
Nijaycrypt22 <ayehanita@gmail.com>
2025-09-11 13:33:59 +0000

    update add page

--- pages/add_event.py
+++ pages/add_event.py
@@ -1,13 +1,26 @@
 from nicegui import ui 
+import requests
+from utils.api import base_url
+
+
+def add_event(data):
+    response=requests.post(f'{base_url}/adverts', data)
+    print(response.json())
 
 def show_add_event_page():
+    flyer_content=None
+
+    def handle_flyer_upload(e):
+        global flyer_content
+        flyer_content = {'name': e.name, 'content': e.content.read(), 'type':e.type}
+        ui.notify(f'uploaded')
+
+
     with ui.column().classes(
-        'max-w-2xl mx-auto mt-24 p-8 rounded-2xl shadow-2xl bg-white space-y-6'
-    ):
+        'max-w-2xl mx-auto mt-24 p-8 rounded-2xl shadow-2xl bg-white space-y-6'):
         # Title
         ui.label('CREATE A NEW ADVERT').classes(
-            'font-extrabold text-4xl tracking-wide text-center text-gray-800'
-        )
+            'font-extrabold text-4xl tracking-wide text-center text-gray-800')
 
         # Inputs
         title = ui.input('Title').props('outlined').classes(
@@ -30,27 +43,30 @@ def show_add_event_page():
 
         # Upload
         ui.label('Upload image').classes('font-medium text-gray-700')
-        flyer = ui.upload(
-            on_upload=lambda e: ui.notify('Uploaded'),
-            on_rejected=lambda: ui.notify('Rejected!'),
-            max_file_size=5_000_000
-        ).classes('w-full rounded-lg border border-dashed border-gray-400 p-4')
+        flyer = ui.upload(on_upload=handle_flyer_upload).classes('w-full rounded-lg border border-dashed border-gray-400 p-4')
 
         # Submit button
-        def submit():
-            ad_data={
-                'title':title.value,
-                'description':description.value,
-                'price':price.value,
-                'category':categories.value,
+        #def submit():
+            #ad_data={
+                #'title':title.value,
+                #'description':description.value,
+                #'price':price.value,
+                #'category':categories.value,
+                
 
-            }
-            print(ad_data)
+            #}
+           # print(ad_data)
+        def submit():
             ui.notify(
                 f"Ad Submitted: {title.value}, {description.value}, {price.value}, {categories.value}"
             )
 
-        ui.button('Submit', on_click=submit).classes(
+        ui.button('Submit', on_click=lambda:add_event({
+            'title':title.value,
+                'description':description.value,
+                'price':price.value,
+                'category':categories.value,
+                'flyer': flyer_content})).classes(
             'mt-6 bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-3 px-6 rounded-xl shadow-md w-full'
         )
 
---
+++ utils/api.py
@@ -0,0 +1,1 @@
+base_url='https://advertisement-api-0myf.onrender.com'
