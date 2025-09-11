84668576434c447ee72a18984747dd4a108097d3
rukkyy <rukayamohammed448@gmail.com>
2025-09-11 09:58:05 +0000

    Changes

--- pages/add_event.py
+++ pages/add_event.py
@@ -9,44 +9,57 @@ def show_add_event_page():
             'font-extrabold text-4xl tracking-wide text-center text-gray-800'
         )
 
-        # Title input
-        ui.input('Title').props('outlined').classes(
+        # Inputs
+        title = ui.input('Title').props('outlined').classes(
             'w-full rounded-lg border-gray-300 focus:ring-2 focus:ring-indigo-500'
         )
 
-        
-        ui.textarea('Item Description').props('outlined').classes(
+        description = ui.textarea('Item Description').props('outlined').classes(
             'w-full rounded-lg border-gray-300 focus:ring-2 focus:ring-indigo-500'
         )
 
-        # Price
-        ui.number('Price').props('outlined').classes(
+        price = ui.number('Price').props('outlined').classes(
             'w-full rounded-lg border-gray-300 focus:ring-2 focus:ring-indigo-500'
         )
 
         # Categories
         ui.label('Categories').classes('font-medium text-gray-700')
-        ui.select(
+        categories = ui.select(
             ['CLOTHINGS', 'TUMBLERS', 'CARS', 'FURNITURE']
         ).classes("w-full rounded-lg").props('outlined')
 
         # Upload
         ui.label('Upload image').classes('font-medium text-gray-700')
-        ui.upload(
-            on_upload=lambda e: ui.notify(f'Uploaded'),
+        flyer = ui.upload(
+            on_upload=lambda e: ui.notify('Uploaded'),
             on_rejected=lambda: ui.notify('Rejected!'),
             max_file_size=5_000_000
         ).classes('w-full rounded-lg border border-dashed border-gray-400 p-4')
 
         # Submit button
         def submit():
-            ui.notify(f'Add Submitted Successfully')
+            ad_data={
+                'title':title.value,
+                'description':description.value,
+                'price':price.value,
+                'category':categories.value,
+
+            }
+            print(ad_data)
+            ui.notify(
+                f"Ad Submitted: {title.value}, {description.value}, {price.value}, {categories.value}"
+            )
 
         ui.button('Submit', on_click=submit).classes(
             'mt-6 bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-3 px-6 rounded-xl shadow-md w-full'
         )
+
+        # Delete button
         def delete():
-            ui.notify(f'Deleted Successfully')
+            ui.notify('Deleted Successfully')
+
         ui.button('Delete', on_click=delete).classes(
-            'mt-6 bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-3 px-6 rounded-xl shadow-md w-full'
+            'mt-3 bg-red-600 hover:bg-red-700 text-white font-semibold py-3 px-6 rounded-xl shadow-md w-full'
         )
+
+
