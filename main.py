1b6e05974c52055a6733d4df79a4d4b029fb5564
rukkyy <rukayamohammed448@gmail.com>
2025-09-11 16:45:05 +0000

    Changes

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ def add_event_page():
     show_add_event_page()
 
 @ui.page("/view_event")
-def view_event_page():
+def view_event_page(id=""):
     show_header()
-    show_view_page()
+    show_view_page(id)
 
 @ui.page("/edit_event")
 def edit_event_page():
--- pages/view_event.py
+++ pages/view_event.py
@@ -1,15 +1,27 @@
-from nicegui import ui 
+from nicegui import ui
+import requests
+from utils.api import base_url
 
 
-def show_view_page():
+def show_view_page(id):
+    response = requests.get(f"{base_url}/adverts/{id}")
+    json_data = response.json()
+    advert = json_data["data"]
+
+    def delete_advert():
+        response = requests.delete(f"{base_url}/adverts/{id}")
+        if response.status_code == 200:
+            ui.navigate.to("/")
+
     with ui.card().classes("w-full h-full sm:w-[90%] md:w-[70%] lg:w-[60%] mx-auto p-6 bg-white rounde-xl shadow-lg mt-24"):
-        ui.image("Assets/furniture-5966893_1280.jpg").classes("w-full h-full rounded-lg mb-4 shadow-xl transition-transform duration-300 hover:scale-110")
-        ui.label("Single Sofa").classes("text-xl font-semibold")
-        ui.label("price:ghc 150").classes("text-gray-700 text-xl shadow-xl")
+        ui.image(advert["flyer"]).classes("w-full h-full rounded-lg mb-4 shadow-xl transition-transform duration-300 hover:scale-110")
+        ui.label(advert["title"]).classes("text-xl font-semibold")
+        ui.label(f"price:ghc {advert["price"]}").classes("text-gray-700 text-xl shadow-xl")
         ui.label("Category: Furniture").classes("text-xl")
         ui.label("Description: A single mordern sofa with plush cushions-Elastic silky fiber fabric. Available in different colors. Gentle on skin and sutable for every place being it office,hall or bedroom. very good for elderlys that needs a good rest time." \
         "visit shop for a 50% discount ").classes("text-grey-600 text-xl")
         ui.button("Buy Item").classes("w-full self-center")
         url = 'http://127.0.0.1:8080/'
         ui.button('Exit', on_click=lambda: ui.navigate.to(url)).classes("self-center")
+        ui.button('Delete', on_click=delete_advert).classes("bg-black text-white px-4 py-2 rounded-lg")
         
